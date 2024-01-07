16dfa9b3b2e0c30602dac66dd2b53d4dfc67e2c8
thejaviertc <javier.toribiocouz@gmail.com>
2024-01-07 23:19:02 +0100

    Add User and Addon structs

---
+++ src/Models/Addon.cs
@@ -0,0 +1,22 @@
+﻿namespace SteamWorkshopStats.Models;
+
+public struct Addon
+{
+    public string Id { get; init; }
+
+    public string Title { get; init; }
+
+    public string ImageUrl { get; init; }
+
+    public int Views { get; init; }
+
+    public int Suscribers { get; init; }
+
+    public int Favorites { get; init; }
+
+    public int Likes { get; init; }
+
+    public int Dislikes { get; init; }
+
+    public int Stars { get; init; }
+}
---
+++ src/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace SteamWorkshopStats.Models;
+
+public struct User
+{
+    public string SteamId { get; init; }
+
+    public string Username { get; init; }
+
+    public string ProfileImageUrl { get; init; }
+
+    public int Views { get; init; }
+
+    public int Suscribers { get; init; }
+
+    public int Favorites { get; init; }
+
+    public int Likes { get; init; }
+
+    public int Dislikes { get; init; }
+
+    public List<Addon> Addons { get; init; }
+}
